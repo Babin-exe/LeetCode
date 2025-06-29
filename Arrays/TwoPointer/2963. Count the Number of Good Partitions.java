01e4347b0312dc86c212f80ab9b30a9821e4fe4f
Babin <155212596+Babin-exe@users.noreply.github.com>
2025-06-29 21:58:24 +0545

    Update 2963. Count the Number of Good Partitions.java

--- Arrays/TwoPointer/2963. Count the Number of Good Partitions.java
+++ Arrays/TwoPointer/2963. Count the Number of Good Partitions.java
@@ -4,29 +4,28 @@ class Solution {
         int i = 0;
         int j = 0;
         int n = nums.length;
-        int count = 0;
-        
 
+        int result = 1;
+        int MOD = 1_000_000_007;
         Map<Integer, Integer> map = new HashMap<>();
         for (int k = 0; k < n; k++) {
             map.put(nums[k], k);
         }
+        j = map.get(nums[0]);
 
         while (i < n) {
-            j = map.get(nums[i]);
-            while (i < j) {
-                j = Math.max(j, map.get(nums[i]));
-                i++;
+            if (i > j) {
+                result = (result * 2) % MOD;
+
             }
+
+            j = Math.max(j,map.get(nums[i]));
             i++;
-            count++;
-        }
-         final int MOD = 1_000_000_007;
-        long res = 1;
-        for (int x = 0; x < count - 1; x++) {
-            res = (res * 2) % MOD;
+
         }
 
-        return (int) res;
+        return result;
     }
 }
+//Time complexity: o(n) because we access the array once 
+//Space complexity is also o(n) because we are building a hashmap of size n
