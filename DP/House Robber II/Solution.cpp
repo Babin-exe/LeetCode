d4c3821589394f9eb68867dc633a8ed65e3a2c33
Baaabin <155212596+Babin-exe@users.noreply.github.com>
2026-09-02 20:59:20 +0545

    Update Solution.cpp

--- DP/House Robber II/Solution.cpp
+++ DP/House Robber II/Solution.cpp
@@ -1,4 +1,6 @@
 // Problem Link :https://leetcode.com/problems/house-robber-ii/
+
+// Recursion + memo
 class Solution {
 public:
     int dp[100];
@@ -34,3 +36,37 @@ if(n==1) return nums[0];
         return max(take_last,take_second_last);
     }
 };
+
+//Bottom up Version : 
+class Solution {
+public:
+    int dp[101];
+    int n;
+    int rob(vector<int>& nums) {
+        n = nums.size();
+
+        if(n==1) return nums[0];
+        memset(dp, 0, sizeof(dp));
+
+        for (int i = 0; i <= n - 2; i++) {
+            int take = nums[i] + (i - 2 >= 0 ? dp[i - 2] : 0);
+            int ntake = i - 1 >= 0 ? dp[i - 1] : 0;
+            dp[i] = max(take, ntake);
+        }
+
+        int case1 = dp[n - 2];
+
+        memset(dp, 0, sizeof(dp));
+        for (int i = 1; i <= n - 1; i++) {
+            int take = nums[i] + (i - 2 >= 1 ? dp[i - 2] : 0);
+            int ntake = i - 1 >= 1 ? dp[i - 1]:0;
+
+            dp[i] = max(take,ntake);
+        }
+
+        int case2 = dp[n - 1];
+
+        return max(case1, case2);
+    }
+};
+
