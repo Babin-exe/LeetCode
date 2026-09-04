5ebf57438ca2b6d02ad12973ce8c7e9838cd958c
Baaabin <155212596+Babin-exe@users.noreply.github.com>
2026-09-04 12:41:12 +0545

    Update Solution.cpp

--- DP/Maximum Alternating Subsequence Sum/Solution.cpp
+++ DP/Maximum Alternating Subsequence Sum/Solution.cpp
@@ -27,3 +27,19 @@ public:
         return solve(0, 1, nums);
     }
 };
+
+//Bottom up : 
+class Solution {
+public:
+    long long maxAlternatingSum(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<long long>> dp(n + 1, vector<long long>(2, 0));
+
+        for (int i = n - 1; i >= 0; --i) {
+            dp[i][1] = max(dp[i + 1][0] + nums[i], dp[i + 1][1]);
+            dp[i][0] = max(dp[i + 1][1] - nums[i], dp[i + 1][0]);
+        }
+
+        return dp[0][1];
+    }
+};
